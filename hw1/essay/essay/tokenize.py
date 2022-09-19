4ddb3d6b65f01cad53976721b4ff0679f23ac784
muyiWang <Whywmy@outlook.com>
2022-09-19 22:19:01 +0800

    add lab1

--- hw1/essay/essay/tokenize.py
+++ hw1/essay/essay/tokenize.py
@@ -1,9 +1,14 @@
+import re
+
 import jieba
+
+
+
 
 if __name__ == '__main__':
 
-    file_in = open('D:/Code/big_data/essay/pure_text.txt', 'r', encoding='utf-8')
-    file_out = open('D:/Code/big_data/essay/text_tokenized.txt', 'w', encoding='utf-8')
+    file_in = open('D:/Code/big_data/bigdata/hw1/essay/pure_text.txt', 'r', encoding='utf-8')
+    file_out = open('D:/Code/big_data/bigdata/hw1/essay/text_tokenized.txt', 'w', encoding='utf-8')
     lines = file_in.readlines()
     i = 0
     for line in lines:
@@ -13,5 +18,9 @@ if __name__ == '__main__':
         print(i)
         file_out.write(seg)
 
+
     file_in.close()
     file_out.close()
+
+
+
